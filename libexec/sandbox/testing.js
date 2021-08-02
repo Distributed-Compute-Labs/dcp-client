3c13e9e0bfdbd84719ad6c9adfeba43fb1a5d3e6
Ryan <ryansaweczko@kingsds.network>
2021-08-02 15:37:38 -0400

    improve explanation for testing file for evaluators

--- libexec/sandbox/testing.js
+++ libexec/sandbox/testing.js
@@ -1,1 +1,3 @@
-// Add your own testing code
+// Add your own testing code. To run this script in the evaluator, either start a
+// stand alone evaluator with the flag `-t testing`, or start a node evaluator of type
+// nodeTesting. That is done automatically in the testing scripts in dcp/tests/api/worker
