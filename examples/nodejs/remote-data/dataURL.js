c81a076a26ddbbaeb2b993b93194236a0c29f395
Nazila <nazila@kingsds.network>
2021-09-30 11:06:28 -0400

    Examples for remote data set and pattern

--- examples/nodejs/remote-data/dataURL.js
+++ examples/nodejs/remote-data/dataURL.js
@@ -1,16 +1,29 @@
 #! /usr/bin/env node
-
 /**
- * Deploy a job using URL for input data
+ * @file   dataURL.js - An example on deploying a job using URL for input data
+ * 
+ * @author Nazila Akhavan <nazila@kingsds.network>
+ * @date   Sep. 2021
+ * 
+ * 
  * There are two ways to stringify input data before sending on the wire
  * - JSON: It is a default method
  * - KVIN: if kvin.serialize(inputData)is being used we need to define the Content-Type `res.header("Content-Type", "application/x-kvin")`
  * 
  * Note that to allow workers fetch data from URLs, 
- *  - in the browser worker: in the console run `dcpConfig.worker.allowOrigins.any.push('http://localhost:12345', 'http://localhost:12346')`
- *  - in the node worker: add `-a 'http://localhost:12345' 'http://localhost:12346'` at the end of starting worker command 
- *  - in the localExec: dcpConfig.worker.allowOrigins.any.push('http://localhost:12345' 'http://localhost:12346');
+ *  - in the node worker: add `-a 'http://localhost:12345' 'http://localhost:12346'` at the end of starting worker command.
+ * 
+ *  - in the localExec: 
+ *    ```dcpConfig = require('dcp/dcp-config');
+ *       dcpConfig.worker.allowOrigins.any.push(['http://localhost:12345' 'http://localhost:12346']);
+ *    ```
+ *  - in the browser worker: in the console run `dcpConfig.worker.allowOrigins.any.push('http://localhost:12345', 'http://localhost:12346')` also,
+ *    you need to add following lines in the response function in slice.get
+ *    ```res.header("Access-Control-Allow-Headers", "content-type");
+ *       res.header("Access-Control-Allow-Origin", "*");
+ *    ``` 
  */
+
 const SCHEDULER_URL = new URL('https://scheduler.distributed.computer');
 const kvin = require('kvin');
 const express = require('express');
@@ -23,8 +36,6 @@ async function main() {
   const slice1 = express();
   const port1 = 12345;
   slice1.get('/', (req, res) => {
-    res.header("Access-Control-Allow-Headers", "content-type")
-    res.header("Access-Control-Allow-Origin", "*")
     let a = {x:1, y:2};
     res.send(a);
   })
@@ -36,8 +47,6 @@ async function main() {
   const slice2 = express();
   const port2 = 12346;
   slice2.get('/', (req, res) => {
-    res.header("Access-Control-Allow-Headers", "content-type")
-    res.header("Access-Control-Allow-Origin", "*")
     let a = {x:1, y:2};
     res.header("Content-Type", "application/x-kvin");
     res.send(kvin.serialize(a));
@@ -77,12 +86,12 @@ async function main() {
 
   job.public.name = 'URL-data-example';
 
-  const results = await job.exec(0.0000001);
+  const results = await job.exec(compute.marketValue);
   console.log('results=', Array.from(results));
  }
  
  /* Initialize DCP Client and run main() */
-require('../..')
+require('../../..')
   .init(SCHEDULER_URL)
   .then(main)
   .catch(console.error)
