226e6dabb897a731a8280af4eda0eb6e63751016
Ryan Saweczko <ryansaweczko@kingsds.network>
2022-08-24 10:47:38 -0400

    Evaluator: change 'gpu-timers' to 'unique-timing'

--- libexec/sandbox/unique-timing.js
+++ libexec/sandbox/unique-timing.js
@@ -1,12 +1,18 @@
 /**
- *  @file       gpu-timers.js
+ *  @file       unique-timing.js
  *              Copyright (c) 2022, Distributive, Ltd.
  *              All Rights Reserved. Licensed under the terms of the MIT License.
  *
- *              This file adds wrappers for webGL and webGPU functions so we can measure their GPU time
+ *              This file adds wrappers various classes/functions that may have different requirements in order to accurately time them.
+ *              Includes:
+ *                - timer for webGL functions
+ *                - timer for webGPU functions
+ *                - wrapper to webGPU and WebAssembly functions that may cause the event loop to start from
+ *                  a different thread (ie after WebAssembly compiling) to ensure our CPU timing can pick up
+ *                  and continue proper measurement.
  *
  *  @author     Ryan Saweczko, ryansaweczko@kingsds.network
- *  @date       July 2022
+ *  @date       Aug 2022
  */
 
 /* global WebGPUWindow GPU */
@@ -16,6 +22,51 @@ self.wrapScriptLoading({ scriptName: 'gpu-timers' }, async function gpuTimers$fn
 {
   const webGLTimer = protectedStorage.timers.webGL;
   const webGPUTimer = protectedStorage.timers.webGPU;
+
+
+  function threadedWrapperFactory(Class)
+  {
+
+    // Iterating through all things 'GPU' on global object, some may not be classes. Skip those without a prototype.
+    if (!self[Class].prototype)
+      return;
+
+    for (let prop of Object.keys(self[Class].prototype))
+    {
+      let originalFn;
+      try
+      {
+        originalFn = self[Class].prototype[prop];
+        if (originalFn instanceof Promise)
+        {
+          originalFn.catch(() => {/* accessing properties from class constructors can be dangerous in weird ways */})
+          continue;
+        }
+        if (typeof originalFn !== 'function')
+          continue;
+      }
+      catch(e)
+      {
+        // The property can't be invoked, so must be a property (like 'name'). Don't need to wrap it.
+        continue;
+      }
+
+      // If the function returns a promise, wrap it with setImmediate. Triggers restart of CPU measurement.
+      self[Class].prototype[prop] = function timerWrapper(...args)
+      {
+        const fn = originalFn.bind(this);
+        var returnValue =  fn(...args);
+        if (returnValue instanceof Promise)
+          return new Promise((resolve, reject) => {
+            returnValue.then(
+              (res) => setImmediate(() => resolve(res)),
+              (rej) => setImmediate(() => reject(rej)));
+          });
+        return returnValue;
+      }
+    }
+  }
+
 
   if (self.OffscreenCanvas && new OffscreenCanvas(1,1))
   {
@@ -62,53 +113,10 @@ self.wrapScriptLoading({ scriptName: 'gpu-timers' }, async function gpuTimers$fn
     const originalSubmit = GPUQueue.prototype.submit;
     const originalSubmitDone = GPUQueue.prototype.onSubmittedWorkDone;
 
-    function webGPUWrapperFactory(webGPUClass)
-    {
-
-      // Iterating through all things 'GPU' on global object, some may not be classes. Skip those without a prototype.
-      if (!self[webGPUClass].prototype)
-        return;
-
-      for (let prop of Object.keys(self[webGPUClass].prototype))
-      {
-        let originalFn;
-        try
-        {
-          originalFn = self[webGPUClass].prototype[prop];
-          if (originalFn instanceof Promise)
-          {
-            originalFn.catch(() => {/* accessing properties from class constructors can be dangerous in weird ways */})
-            continue;
-          }
-          if (typeof originalFn !== 'function')
-            continue;
-        }
-        catch(e)
-        {
-          // The property can't be invoked, so must be a property (like 'name'). Don't need to wrap it.
-          continue;
-        }
-
-        // If the function returns a promise, wrap it with setImmediate. Triggers restart of CPU measurement.
-        self[webGPUClass].prototype[prop] = function webGPU(...args)
-        {
-          const fn = originalFn.bind(this);
-          var returnValue =  fn(...args);
-          if (returnValue instanceof Promise)
-            return new Promise((resolve, reject) => {
-              returnValue.then(
-                (res) => setImmediate(() => resolve(res)),
-                (rej) => setImmediate(() => reject(rej)));
-            });
-          return returnValue;
-        }
-      }
-    }
-
     for (let key of Object.getOwnPropertyNames(self))
     {
       if (key.startsWith('GPU'))
-        webGPUWrapperFactory(key);
+      threadedWrapperFactory(key);
     }
 
     GPUQueue.prototype.submit = function submit(...args)
@@ -125,5 +133,7 @@ self.wrapScriptLoading({ scriptName: 'gpu-timers' }, async function gpuTimers$fn
       })
     }
   }
+
+  debugger;
 
 });
