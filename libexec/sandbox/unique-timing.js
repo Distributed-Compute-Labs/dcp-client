f91460594366b58a2d6689f4514f8516ddc6cde5
Ryan Saweczko <ryansaweczko@kingsds.network>
2022-08-24 10:47:38 -0400

    Evaluator: change 'gpu-timers' to 'unique-timing'

--- libexec/sandbox/unique-timing.js
+++ libexec/sandbox/unique-timing.js
@@ -23,43 +23,49 @@ self.wrapScriptLoading({ scriptName: 'gpu-timers' }, async function gpuTimers$fn
   const webGLTimer = protectedStorage.timers.webGL;
   const webGPUTimer = protectedStorage.timers.webGPU;
 
-  // WebAssembly doesn't have a prototype, can't use the factory the same way. But WebAssembly's spec is finalized so we don't need to be as general as for webGPU
-  for (const prop of Object.keys(WebAssembly))
+  protectedStorage.getAndResetWebGLTimer = function getAndResetWebGLTimer()
   {
-    const fn = WebAssembly[prop];
-    WebAssembly[prop] = function timerWrapper(...args)
-    {
-      var unwrappedReturn =  fn.bind(this)(...args);
-      if (unwrappedReturn instanceof Promise)
-        return new Promise((resolve, reject) => {
-          unwrappedReturn.then(
-            (res) => setImmediate(() => resolve(res)),
-            (rej) => setImmediate(() => reject(rej)));
-        });
-      return unwrappedReturn;
-    }
+    const time = webGLTimer.length;
+    webGLTimer.reset();
+    return time;
   }
 
   /**
-   * Given a class, wrap all functions in that class so.
-   * The wrapper will not change the function if the function returns a value.
-   * If the function returns a promise, wrap the return value with another promise, so that
-   * when the original promise resolves or rejects, setImmediate will be called to resolve the
-   * wrapping promise. This causes the promise resolution to spend a moment on the event loop,
-   * allowing timing code to know the promise resolution occurred.
+   * @returns {boolean} 
    */
-  function wrapPrototypeFunctions(GPUClass)
+  protectedStorage.hasWebglSupport = function webglSupport() {
+    try
+    {
+      const canvas = new OffscreenCanvas(1,1);
+      return Boolean(canvas.getContext('webgl') || canvas.getContext('webgl2'));
+    }
+    catch
+    {
+      return false;
+    }
+  };
+
+  if (protectedStorage.hasWebglSupport())
+    protectedStorage.getAndResetWebGLTimer = function getAndResetWebGLTimer()
+    {
+      const time = webGLTimer.length;
+      webGLTimer.reset();
+      return time;
+    }
+
+  function threadedWrapperFactory(Class)
   {
+
     // Iterating through all things 'GPU' on global object, some may not be classes. Skip those without a prototype.
-    if (!self[GPUClass].prototype)
+    if (!self[Class].prototype)
       return;
 
-    for (let prop of Object.keys(self[GPUClass].prototype))
+    for (let prop of Object.keys(self[Class].prototype))
     {
       let originalFn;
       try
       {
-        originalFn = self[GPUClass].prototype[prop];
+        originalFn = self[Class].prototype[prop];
         if (originalFn instanceof Promise)
         {
           originalFn.catch(() => {/* accessing properties from class constructors can be dangerous in weird ways */})
@@ -75,15 +81,15 @@ self.wrapScriptLoading({ scriptName: 'gpu-timers' }, async function gpuTimers$fn
       }
 
       // If the function returns a promise, wrap it with setImmediate. Triggers restart of CPU measurement.
-      self[GPUClass].prototype[prop] = function timerWrapper(...args)
+      self[Class].prototype[prop] = function timerWrapper(...args)
       {
         const fn = originalFn.bind(this);
-        const returnValue =  fn(...args);
+        var returnValue =  fn(...args);
         if (returnValue instanceof Promise)
           return new Promise((resolve, reject) => {
-            returnValue
-            .then((res) => setImmediate(() => resolve(res)))
-            .catch((rej) => setImmediate(() => reject(rej)));
+            returnValue.then(
+              (res) => setImmediate(() => resolve(res)),
+              (rej) => setImmediate(() => reject(rej)));
           });
         return returnValue;
       }
@@ -92,25 +98,20 @@ self.wrapScriptLoading({ scriptName: 'gpu-timers' }, async function gpuTimers$fn
 
   if (self.OffscreenCanvas && new OffscreenCanvas(1,1))
   {
-    /**
-     *  Wrap webGL function for a given context. The wrapper will add a time interval to the
-     *  webGLTimer that measures the execution time of the function run.
-     * 
-     * @param {obj} context - the OffscreenCanvas context for which a function needs to be wrapped
-     * @param {string} prop - property of the context to be wrapped 
-     */
-    function timeWebGLFunction(context, prop)
+
+    /* Factory to wrap a function from a context with a timer */
+    function timeWebGLFactory(context, prop)
     {
-      const originalFn = context[prop].bind(context);
+      let originalFn = context[prop].bind(context);
       
       context[prop] = function wrappedWebGLFunction(...args)
       {
-        let returnValue;
+        var returnValue;
         const interval = new protectedStorage.TimeInterval();
         webGLTimer.push(interval);
         try
         {
-          returnValue = originalFn(...args);
+          returnValue =  originalFn(...args);
           interval.stop();
         }
         catch(e)
@@ -123,41 +124,111 @@ self.wrapScriptLoading({ scriptName: 'gpu-timers' }, async function gpuTimers$fn
     }
   
     /* Update all functions on the OffscreenCanvas getContext prototype to have timers */
-    const oldGetContext = OffscreenCanvas.prototype.getContext;
+    let oldGetContext = OffscreenCanvas.prototype.getContext;
     OffscreenCanvas.prototype.getContext = function(type, options)
     {
-      const context = oldGetContext.bind(this)(type, options);
-      for (const key of Object.getOwnPropertyNames(context.__proto__))
+      let context = oldGetContext.bind(this)(type, options);
+      for (let key of Object.getOwnPropertyNames(context.__proto__))
         if (typeof context[key] === 'function')
-          timeWebGLFunction(context, key);
+          timeWebGLFactory(context, key);
       return context;
     };
   }
 
-  if (typeof navigator === 'undefined' || !navigator?.gpu)
-    return;
-
-  // Want to use the wrapped versions of these after all gpu functions are wrapped.
-  const originalSubmit = GPUQueue.prototype.submit;
-  const originalSubmitDone = GPUQueue.prototype.onSubmittedWorkDone;
-
-  for (const key of Object.getOwnPropertyNames(self))
+  if (navigator.gpu)
   {
-    if (key.startsWith('GPU'))
-    wrapPrototypeFunctions(key);
+    const originalSubmit = GPUQueue.prototype.submit;
+<<<<<<<< HEAD:libexec/sandbox/unique-timing.js
+    const originalSubmitDone = GPUQueue.prototype.onSubmittedWorkDone;
+
+    for (let key of Object.getOwnPropertyNames(self))
+    {
+      if (key.startsWith('GPU'))
+      threadedWrapperFactory(key);
+    }
+
+|||||||| constructed merge base:libexec/sandbox/gpu-timers.js
+    const originalSubmitDone = GPUQueue.prototype.onSubmittedWorkDone;
+
+    function webGPUWrapperFactory(webGPUClass)
+    {
+
+      // Iterating through all things 'GPU' on global object, some may not be classes. Skip those without a prototype.
+      if (!self[webGPUClass].prototype)
+        return;
+
+      for (let prop of Object.keys(self[webGPUClass].prototype))
+      {
+        let originalFn;
+        try
+        {
+          originalFn = self[webGPUClass].prototype[prop];
+          if (originalFn instanceof Promise)
+          {
+            originalFn.catch(() => {/* accessing properties from class constructors can be dangerous in weird ways */})
+            continue;
+          }
+          if (typeof originalFn !== 'function')
+            continue;
+        }
+        catch(e)
+        {
+          // The property can't be invoked, so must be a property (like 'name'). Don't need to wrap it.
+          continue;
+        }
+
+        // If the function returns a promise, wrap it with setImmediate. Triggers restart of CPU measurement.
+        self[webGPUClass].prototype[prop] = function webGPU(...args)
+        {
+          const fn = originalFn.bind(this);
+          var returnValue =  fn(...args);
+          if (returnValue instanceof Promise)
+            return new Promise((resolve, reject) => {
+              returnValue.then(
+                (res) => setImmediate(() => resolve(res)),
+                (rej) => setImmediate(() => reject(rej)));
+            });
+          return returnValue;
+        }
+      }
+    }
+
+    for (let key of Object.getOwnPropertyNames(self))
+    {
+      if (key.startsWith('GPU'))
+        webGPUWrapperFactory(key);
+    }
+
+========
+>>>>>>>> Evaluator: change 'gpu-timers' to 'unique-timing':libexec/sandbox/gpu-timers.js
+    GPUQueue.prototype.submit = function submit(...args)
+    {
+      const submit = originalSubmit.bind(this);
+      submit(...args);
+
+      const queueP = this.onSubmittedWorkDone();
+      const interval = new protectedStorage.TimeInterval();
+      webGPUTimer.push(interval, queueP);
+
+      queueP.then(() => {
+        interval.stop();
+      })
+    }
+
+    const originalMap = GPUBuffer.prototype.mapAsync;
+    GPUBuffer.prototype.mapAsync = function mapAsync(...args)
+    {
+      const mapAsync = originalMap.bind(this);
+      const p = mapAsync(...args);
+
+      // Use setImmediate to resolve the map to ensure we are able to restart our timing.
+      return new Promise( (resolve, reject) => {
+        p.then(res => setImmediate(() => resolve(res), 0));
+      });
+    }
+
   }
 
-  GPUQueue.prototype.submit = function submit(...args)
-  {
-    const fn = originalSubmit.bind(this);
-    fn(...args);
+  debugger;
 
-    const queueP = originalSubmitDone.bind(this)();
-    const interval = new protectedStorage.TimeInterval();
-    webGPUTimer.push({ interval, queueP });
-
-    queueP.then(() => {
-      interval.stop();
-    });
-  }
 });
