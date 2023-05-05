09b493c5ade12fdb7a9b470ae19287d17cda878a
Ryan Sawezcko <ryansaweczko@distributive.network>
2023-05-05 19:38:19 +0000

    Timing: improve documentation of new timing code

--- libexec/sandbox/timer-classes.js
+++ libexec/sandbox/timer-classes.js
@@ -2,23 +2,24 @@
  *  @file       timer-classes.js
  *              This file creates classes that will be required for timing.
  * 
- * TimeInterval:  measure an interval of time. Time interval starts when the object
- *                is instantiated, and ends when `TimeInterval.stop()` is called.
- *                Its length property is the time duration.
+ * The 4 classes defined are:
+ *  - TimeInterval: measure an interval of time.
+ *  - TimeThing:    generic collection of TimeIntervals
+ *  - TimeCPU:      collection of TimeIntervals, with a reference to the most recent interval.
+ *  - TimeWebGPU:   collection of TimeIntervals, with duration not counting overlap, and awaiting for GPU completion event.
  * 
- * TimeThing:     Generic wrapper for multiple TimeIntervals. Can add new intervals
- *                with `push`, reset all intervals with `reset`, and find the total
- *                time duration (including any overlapping time) with `duration`.
- * 
- * TimeCPU:       Inherits from TimeThing. Adds a reference to the most recently added
- *                duration, so it can be ended even if there is no other available reference
- *                to that interval.
- * 
- * TimeWebGPU:    Inherits from TimeThing. Changes how `duration` functions to:
- *                    a) make it async to ensure all durations are finished before resolving (accomplished
- *                       by keeping a reference to the promise that will finish the last interval)
- *                    b) returns the non-overlapping duration.
- * 
+ * TimeInterval is an object shaped like {start: X, stop: Y} with a few additional functions to stop the interval.
+ * Start is set to the current time immediately when the interval is created, and stop is set when the `stop` function
+ * is called. If the length of an interval is accessed before the interval is stopped, it will throw an error.
+ * A TimeThing contains a possibly-overlapping list of TimeIntervals. It adds a duration function to calculate the total
+ * time of all intervals (double-counting any overlaps). If the Thing may need to have it's final interval stopped in an
+ * area that won't otherwise have a reference to that interval, TimeCPU can be used. TimeCPU only adds a reference to the
+ * most recent interval to be added to the list.
+ * TimeWebGPU is special due to the challenges of timing a GPU from Javascript. It's duration function is async, since
+ * the moment the GPU is finished is unknowable until it reports it's finished. If a work function adds a task to the GPU,
+ * but resolves before the GPU is finished, we need to carefully to wait for the GPU to be finished before the duration
+ * can be calculated. TimeWebGPU has this built-in to the duration function.
+
  * @author  Ryan Saweczko <ryansaweczko@kingsds.network>
  * @date    Aug 2022
  */
@@ -27,7 +28,13 @@
 
 self.wrapScriptLoading({ scriptName: 'timer-classes' }, function timerClasses$$fn(protectedStorage)
 {
-  
+  /**
+   * Time interval class.
+   * 
+   * Contains a start time and end time. The start time is set immediately when the interval is created,
+   * and the end time is set when `stop` is called. Once the interval has been stopped, the length
+   * can be accessed (getting interval.length before it's stopped is an error).
+   */
   function TimeInterval()
   {
     this.start = performance.now();
@@ -43,6 +50,9 @@ self.wrapScriptLoading({ scriptName: 'timer-classes' }, function timerClasses$$f
     }
   });
 
+  /**
+   * Stop a timer. The `end` time is set.
+   */
   TimeInterval.prototype.stop = function stop()
   {
     if (this.end)
@@ -51,19 +61,32 @@ self.wrapScriptLoading({ scriptName: 'timer-classes' }, function timerClasses$$f
     return true;
   }
 
-  TimeInterval.prototype.isEnded = function isEnded()
+  /**
+   * Check if the interval has been stopped (end time has been set)
+   */
+  TimeInterval.prototype.hasEnded = function hasEnded()
   {
     return typeof this.end === 'number';
   };
 
   protectedStorage.TimeInterval = TimeInterval;
 
-  
+  /**
+   * Time Thing class
+   * 
+   * Generic collection of time intervals. Contains a list
+   * of intervals, and provides a way to get the total time
+   * of all intervals.
+   */
   function TimeThing()
   {
     this.intervals = [];
   }
-  
+
+  /**
+   * Get the total length of all intervals. If the intervals are overlapping,
+   * the overlapping time will be counted twice.  
+   */
   TimeThing.prototype.duration = function totalDuration()
   {
     let sum = 0;
@@ -72,16 +95,30 @@ self.wrapScriptLoading({ scriptName: 'timer-classes' }, function timerClasses$$f
     return sum;
   }
 
-  TimeThing.prototype.push = function push(ele)
+  /**
+   * Add a new interval.
+   * 
+   * @Param {TimeInterval} interval - new interval to add to the collection
+   */
+  TimeThing.prototype.push = function push(interval)
   {
-    this.intervals.push(ele);
+    this.intervals.push(interval);
   }
 
+  /**
+   * Reset the interval. Resets the interval list to an empty list.
+   */
   TimeThing.prototype.reset = function reset()
   {
     this.intervals = [];
   }
 
+  /**
+   * Time CPU class
+   * 
+   * Inherits from TimeThing, but adds a reference to the most recent interval
+   * to be added.
+   */
   function TimeCPU()
   {
     TimeThing.call(this);
@@ -89,12 +126,23 @@ self.wrapScriptLoading({ scriptName: 'timer-classes' }, function timerClasses$$f
   }
   TimeCPU.prototype = new TimeThing();
 
+  /**
+   * Add a new interval. `this.mostRecentInterval` is set to this interval.
+   * 
+   * @Param {TimeInterval} interval - new interval to add to the collection
+   */
   TimeCPU.prototype.push = function push(ele)
   {
     this.intervals.push(ele);
     this.mostRecentInterval = ele;
   }
 
+  /**
+   * Time WebGPU class
+   * 
+   * Inherits from TimeThing, but adds a reference to the most recent interval,
+   * as well as large changes to the duration function to handle overlapping time intervals.
+   */
   function TimeWebGPU()
   {
     TimeThing.call(this);
@@ -102,6 +150,13 @@ self.wrapScriptLoading({ scriptName: 'timer-classes' }, function timerClasses$$f
   }
   TimeWebGPU.prototype = new TimeThing();
 
+  /**
+   * Add a new interval. `this.mostRecentInterval` is set to this interval.
+   * 
+   * @Param {object} interval - {interval: TimeInterval, queueP: Promise}
+   *                            new interval to add to the collection, as well as a corresponding promise
+   *                            for when the most recently submitted webGPU queue is finished.
+   */
   TimeWebGPU.prototype.push = function push(obj)
   {
     this.intervals.push(obj.interval);
@@ -109,12 +164,21 @@ self.wrapScriptLoading({ scriptName: 'timer-classes' }, function timerClasses$$f
   }
 
   /**
-   * How long was spent in the gpu.
+   * Measure time (in ms) spent in the gpu. Timing is done ignoring overlaps - 
+   * so if interval 1 was from 0-5 seconds and interval 2 was from 3-8 seconds,
+   * the returned duration will be 8.
    * 
-   * The returned promise will only resolve once all webGPU code has run to completion.
+   * Furthermore, since it's impossible to know when webGPU code execution is finished until
+   * the GPU reports it's finished, awaiting the last `onSubmittedWorkDone` promise is required.
+   * A loop is used here in case the webGPU finishing triggers anything else on the microtask queue
+   * 
+   * @returns - promise that will resolve with the total duration once all webGPU code has run to completion.
    */
   TimeWebGPU.prototype.duration = async function duration()
   {
+    var totalTime = 0;
+    var previousEnd = 0;
+
     while (this.latestWebGPUCall)
     {
       const latestCall = this.latestWebGPUCall;
@@ -123,24 +187,36 @@ self.wrapScriptLoading({ scriptName: 'timer-classes' }, function timerClasses$$f
         this.latestWebGPUCall = null;
     }
 
-    let totalTime = 0;
-    let previousEnd = 0;
     for (let interval of this.intervals)
     {
-      if (previousEnd > interval.start)
+      if (previousEnd <= interval.start)
+        totalTime += interval.length
+      else
       {
-        if (!interval.isEnded())
+        if (!interval.hasEnded())
           throw new Error("Invalid length: interval hasn't been stopped");
         totalTime += interval.end - previousEnd;
       }
-      else
-        totalTime += interval.length
-      
       previousEnd = interval.end;
     }
     return totalTime;
   }
 
+  /**
+   * Instantiate a timer for each type of execution we need to measure.
+   * 
+   * cpu: measure total CPU usage of a slice. This measurement is based on
+   *      the state of the event loop, and when any specific JavaScript code
+   *      block is executing
+   * webGPU: measure the amount of time between `submitting` a command queue, and
+   *         an onSubmittedWorkDone promise resolving to signal all work sent to the GPU
+   *         is finished. These measurements are as close as we can get in JavaScript to
+   *         the exact duration of the execution of code on the GPU using webGPU.
+   * webGL: measure the amount of time spent in a webGL function. Since webGL is synchronously
+   *        executed, this time is indistinguishable from CPU time measured (unless all webGL
+   *        functions also stop/start new CPU timing), so we have to subtract this measured
+   *        time from CPU time to get the actual CPU time.
+   */
   protectedStorage.timers = {
     cpu:    new TimeCPU(),
     webGPU: new TimeWebGPU(),
--- libexec/sandbox/unique-timing.js
+++ libexec/sandbox/unique-timing.js
@@ -23,20 +23,43 @@ self.wrapScriptLoading({ scriptName: 'gpu-timers' }, async function gpuTimers$fn
   const webGLTimer = protectedStorage.timers.webGL;
   const webGPUTimer = protectedStorage.timers.webGPU;
 
-
-  function threadedWrapperFactory(Class)
+  // WebAssembly doesn't have a prototype, can't use the factory the same way. But WebAssembly's spec is finalized so we don't need to be as general as for webGPU
+  for (const prop of Object.keys(WebAssembly))
   {
+    const fn = WebAssembly[prop];
+    WebAssembly[prop] = function timerWrapper(...args)
+    {
+      var returnValue =  fn.bind(this)(...args);
+      if (returnValue instanceof Promise)
+        return new Promise((resolve, reject) => {
+          returnValue.then(
+            (res) => setImmediate(() => resolve(res)),
+            (rej) => setImmediate(() => reject(rej)));
+        });
+      return returnValue;
+    }
+  }
 
+  /**
+   * Given a class, wrap all functions in that class so.
+   * The wrapper will not change the function if the function returns a value.
+   * If the function returns a promise, wrap the return value with another promise, so that
+   * when the original promise resolves or rejects, setImmediate will be called to resolve the
+   * wrapping promise. This causes the promise resolution to spend a moment on the event loop,
+   * allowing timing code to know the promise resolution occurred.
+   */
+  function wrapPrototypeFunctions(GPUClass)
+  {
     // Iterating through all things 'GPU' on global object, some may not be classes. Skip those without a prototype.
-    if (!self[Class].prototype)
+    if (!self[GPUClass].prototype)
       return;
 
-    for (let prop of Object.keys(self[Class].prototype))
+    for (let prop of Object.keys(self[GPUClass].prototype))
     {
       let originalFn;
       try
       {
-        originalFn = self[Class].prototype[prop];
+        originalFn = self[GPUClass].prototype[prop];
         if (originalFn instanceof Promise)
         {
           originalFn.catch(() => {/* accessing properties from class constructors can be dangerous in weird ways */})
@@ -52,7 +75,7 @@ self.wrapScriptLoading({ scriptName: 'gpu-timers' }, async function gpuTimers$fn
       }
 
       // If the function returns a promise, wrap it with setImmediate. Triggers restart of CPU measurement.
-      self[Class].prototype[prop] = function timerWrapper(...args)
+      self[GPUClass].prototype[prop] = function timerWrapper(...args)
       {
         const fn = originalFn.bind(this);
         const returnValue =  fn(...args);
@@ -67,12 +90,16 @@ self.wrapScriptLoading({ scriptName: 'gpu-timers' }, async function gpuTimers$fn
     }
   }
 
-
   if (self.OffscreenCanvas && new OffscreenCanvas(1,1))
   {
-
-    /* Factory to wrap a function from a context with a timer */
-    function timeWebGLFactory(context, prop)
+    /**
+     *  Wrap webGL function for a given context. The wrapper will add a time interval to the
+     *  webGLTimer that measures the execution time of the function run.
+     * 
+     * @param {obj} context - the OffscreenCanvas context for which a function needs to be wrapped
+     * @param {string} prop - property of the context to be wrapped 
+     */
+    function timeWebGLFunction(context, prop)
     {
       const originalFn = context[prop].bind(context);
       
@@ -102,53 +129,35 @@ self.wrapScriptLoading({ scriptName: 'gpu-timers' }, async function gpuTimers$fn
       const context = oldGetContext.bind(this)(type, options);
       for (const key of Object.getOwnPropertyNames(context.__proto__))
         if (typeof context[key] === 'function')
-          timeWebGLFactory(context, key);
+          timeWebGLFunction(context, key);
       return context;
     };
   }
 
-  if (navigator.gpu)
+  if (!navigator.gpu)
+    return
+
+  // Want to use the wrapped versions of these after all gpu functions are wrapped.
+  const originalSubmit = GPUQueue.prototype.submit;
+  const originalSubmitDone = GPUQueue.prototype.onSubmittedWorkDone;
+
+  for (const key of Object.getOwnPropertyNames(self))
   {
-    // Want to use the wrapped versions of these after all gpu functions are wrapped.
-    const originalSubmit = GPUQueue.prototype.submit;
-    const originalSubmitDone = GPUQueue.prototype.onSubmittedWorkDone;
-
-    for (const key of Object.getOwnPropertyNames(self))
-    {
-      if (key.startsWith('GPU'))
-      threadedWrapperFactory(key);
-    }
-
-    GPUQueue.prototype.submit = function submit(...args)
-    {
-      const fn = originalSubmit.bind(this);
-      fn(...args);
-
-      const queueP = originalSubmitDone.bind(this)();
-      const interval = new protectedStorage.TimeInterval();
-      webGPUTimer.push({ interval, queueP });
-
-      queueP.then(() => {
-        interval.stop();
-      })
-    }
+    if (key.startsWith('GPU'))
+    wrapPrototypeFunctions(key);
   }
 
-  // WebAssembly doesn't have a prototype, can't use the factory the same way. But WebAssembly's spec is finalized so we don't need to be as general as for webGPU
-  for (const prop of Object.keys(WebAssembly))
+  GPUQueue.prototype.submit = function submit(...args)
   {
-    const fn = WebAssembly[prop];
-    WebAssembly[prop] = function timerWrapper(...args)
-    {
-      var returnValue =  fn.bind(this)(...args);
-      if (returnValue instanceof Promise)
-        return new Promise((resolve, reject) => {
-          returnValue.then(
-            (res) => setImmediate(() => resolve(res)),
-            (rej) => setImmediate(() => reject(rej)));
-        });
-      return returnValue;
-    }
-  }
+    const fn = originalSubmit.bind(this);
+    fn(...args);
 
+    const queueP = originalSubmitDone.bind(this)();
+    const interval = new protectedStorage.TimeInterval();
+    webGPUTimer.push({ interval, queueP });
+
+    queueP.then(() => {
+      interval.stop();
+    });
+  }
 });
