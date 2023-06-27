30804047d50844cc0ccde87de6e2c0f83149ea63
Liang Wang <liang@distributive.network>
2023-06-27 22:38:16 +0000

    re-invent the cancelation token wheel
    I strongly suspect I'm actually doing something wrong if I end up
    wanting all the classic concurrency primitives back

--- libexec/sandbox/bravojs-env.js
+++ libexec/sandbox/bravojs-env.js
@@ -254,7 +254,6 @@ self.wrapScriptLoading({ scriptName: 'bravojs-env', ringTransition: true }, func
     try { flushLastLog(); } catch(e) {};
     try
     {
-      // debugger;
       protectedStorage.lockTimers(); // lock timers so no new timeouts will be run.
       // TODO: think about which setTimeout to use, it seems if the user uses up resources on the machine, they should
       // be charged, even for lingering microtasks. After all, most likely we didn't intentionally cause them.
