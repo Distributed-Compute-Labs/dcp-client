1671b69ac3d55df22215df995712d18a7c0f5450
Liang Wang <liang@distributive.network>
2023-07-06 19:00:33 +0000

    Update `timed-promise.js` comments to reflect reality

--- libexec/sandbox/timed-promise.js
+++ libexec/sandbox/timed-promise.js
@@ -1,11 +1,8 @@
 /**
  *  @file       timed-promise.js
  *
- *              A timed promise is a promise that will measure how long it took to resolve within our best abilities.
- *              This class is *meant* to be expandable in the future! What you need to modify is the switch statement
- *              for `originTag`, think of it as a sum type and you need to match every variant.
- *
- *              This class exists so we can measure resource usage for thing that happen off the main CPU thread.
+ *              A timed promise is a promise that will measure how long the construction and continuation takes on the
+ *              CPU. See the header comment in event-loop-virtualization for the theory behind its working.
  *
  *  @author     Liang Wang, liang@distributive.network
  *  @date       2023
@@ -14,7 +11,6 @@
 
 self.wrapScriptLoading({ scriptName: 'timed-promise' }, function timedPromise(protectedStorage)
 {
-  /** @todo think about how to not use global variables */
   const TimeInterval = protectedStorage.TimeInterval;
   const cpuIntervals = protectedStorage.bigBrother.globalTrackers.cpuIntervals;
 
@@ -47,16 +43,7 @@ self.wrapScriptLoading({ scriptName: 'timed-promise' }, function timedPromise(pr
     }
   }
   /**
-   * TODO: actually think about how this is used and what are the implications
-   *
-   * A TimedPromise is a promise that records how long it took to *resolve*. It fulfills the thennable interface so it
-   * smells like a promise, acts like a promise, and can be used like a promise. In most cases, you should only use
-   * this for promises that cause of the fulfillment happens due to off thread work (e.g. WebGPU, WebAssembly, WebGL,
-   * etc).
-   *
-   * A notable feature of this class that we will kick the continuation to an timeout so we can record the time it took
-   * for the *continuation* to run.
-   *
+   * A Promise that will record how long the construction and continuation takes. It has all the same API as the standard Promise
    * @class TimedPromise
    */
   class TimedPromise
@@ -77,56 +64,12 @@ self.wrapScriptLoading({ scriptName: 'timed-promise' }, function timedPromise(pr
     }
 
     /**
-     * WARNING: this is basically a constructor 
+     * Implements the thennable interface, so we can chain promises and await on them.
      *
-     * Goal: some functions return a promise already, can't change that, best we can do is fake it
-     * @param {GlobalTrackers} globalTimers
-     * @param {() => Promise} promiseFn
-     * @param {(duration) => undefined} promiseFn
-     * @returns {TimedPromise}
-     */
-    fromExistingPromiseFunction(promiseFn, recordTime)
-    {
-      this.recordTime = recordTime;
-      const that = this;
-
-      this.wrapped = new RealPromise((resolve, reject) =>
-      {
-        // our queueMicrotask know show to measure
-        queueMicrotask(() =>
-        {
-          const duration = new TimeInterval();
-
-          try
-          {
-            const promise = promiseFn();
-            duration.stop();
-
-            that.recordTime(duration);
-            resolve(promise);
-          }
-          catch (error)
-          {
-            duration.stop();
-
-            that.recordTime(duration);
-            reject(error);
-          }
-        })
-      });
-    }
-
-
-    /**
-     * Implements the thennable interface, so we can chain promises and async await on them. In general, we want to kick
-     * off another round of event loop here to get our continuation timed.
-     *
-     *
-     * @todo update doc to support values instead of only functions
      * @function then
-     * @param {Function} onFulfilled
-     * @param {Function} onRejected
-     * @returns {Thennable} go read MDN about what is a thennable 🙂
+     * @param {Function | value} onFulfilled
+     * @param {Function | value} onRejected
+     * @returns {Thennable} go read MDN about what is a thennable
      */
     then(onFulfilled, onRejected)
     {
