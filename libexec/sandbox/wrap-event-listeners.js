97db6da38f3b1f4b58e3d57e74dfb34f7ffbee7e
Severn Lortie <severn@distributive.network>
2023-12-21 14:07:32 -0500

    wrap-event-listeners.js - better event handling

--- libexec/sandbox/wrap-event-listeners.js
+++ libexec/sandbox/wrap-event-listeners.js
@@ -7,6 +7,7 @@
  * 
  *
  *  @author     Ryan Saweczko, ryansaweczko@kingsds.network
+ *  @author     Severn Lortie <severn@distributive.network>
  *  @date       Oct 2021
  *
  */
@@ -15,24 +16,56 @@ self.wrapScriptLoading({ scriptName: 'event-loop-virtualization' }, function wra
   // Will be removing KVIN in access-lists.js, so need an alias for them
   var unmarshal = KVIN.unmarshal
 
-  const globalAddEventListener = self.addEventListener;
-  self.addEventListener = function workerControl$$Worker$addEventListener (type, listener) {
-    if (type === 'message')
+  const eventListeners = [];
+  /**
+   * Call registered event handlers when a message from the main thread arrives at the worker. If the MessageEvent has
+   * a data marshaled data property, this method will unmarhsal it before passing to handlers. For performance, it is
+   * important that the message is unmarhsaled only once for all of the listeners.
+   * @param {MessageEvent} messageEvent The MessageEvent from the main thread
+   */
+  function workerControl$$Worker$handleMessageEvent(messageEvent)
+  {
+    let unmarshaledData;
+    if (messageEvent && messageEvent.data && messageEvent.data._serializeVerId)
+      unmarshaledData = unmarshal(messageEvent.data);
+    // Just like the baked-in MessageEvents, the unmarshaledData object should be immutable. This is also important since
+    // all listeners will have a reference to the same data.
+    Object.freeze(unmarshaledData);
+    for (const eventListener of eventListeners)
     {
-      const wrappedListener = (args) => {
-        if (args.data && args.data._serializeVerId)
-        {
-          args = unmarshal(args.data)
-        }
-        listener(args);
-      }
-      globalAddEventListener(type, wrappedListener);
+      if (unmarshaledData)
+        eventListener(unmarshaledData);
+      else
+        eventListener(messageEvent);
     }
-    else
-    {
-      globalAddEventListener(type, listener);
-    }
-    
   }
 
+  const globalAddEventListener    = self.addEventListener;
+  const globalRemoveEventListener = self.removeEventListener;
+  /**
+   * Add an event listener. If the listener is for the "message" event, then any marshaled data will be ummarshaled before being
+   * passed to the handler. 
+   * @param {string} event The name of the event to listen on
+   * @param {function} listener The handler function
+   */
+  self.addEventListener = function workerControl$$Worker$addEventListener (event, listener)
+  {
+    if (event === 'message')
+      eventListeners.push(listener);
+    else
+      globalAddEventListener(event, listener);
+  }
+  /**
+   * Remove an event listener.
+   * @param {string} event The name of the event that the handler listens on
+   * @param {function} listener The handler function
+   */
+  self.removeEventListener = function workerControl$$Worker$removeEventListener(event, listener)
+  {
+    if (event === 'message')
+      eventListeners.splice(eventListeners.indexOf(listener), 1);
+    else
+      globalRemoveEventListener(event, listener);
+  }
+  globalAddEventListener('message', workerControl$$Worker$handleMessageEvent);
 });
