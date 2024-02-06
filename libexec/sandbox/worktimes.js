8c34ba72e92921852630f61380c72fd8514972f3
Will Pringle <will@distributive.network>
2024-02-06 06:43:24 -0500

    refactor(worktimes.js): add date and clean up file

--- libexec/sandbox/worktimes.js
+++ libexec/sandbox/worktimes.js
@@ -1,50 +1,50 @@
 /**
  * @file      worktimes.js
  *            Specify available worktimes, allow registering custom worktimes
+ *            The single source of authority for what Worktimes are available.
  * 
- * @author    Will Pringle, will@distributive.network
- *            Hamada Gasmallah, hamada@distributive.network
+ * @author    Will Pringle     <will@distributive.network>
+ *            Hamada Gasmallah <hamada@distributive.network>
+ * @date      January 2024
  */
 'use strict';
 
 function worktimes$$fn(protectedStorage, _ring2PostMessage)
 {
+  // when preparing a worktime, add it's globals to this object.
+  // only if the job assigned to the evaluator uses that worktime, they will
+  // be added to the allow-list
+  protectedStorage.worktimeGlobals = {};
+  protectedStorage.legacyArrayWorktimeFormat = legacyArrayWorktimeFormat;
 
-// when preparing a worktime, add it's globals to this object.
-// only if the job assigned to the evaluator uses that worktime, they will
-// be added to the allow-list
-protectedStorage.worktimeGlobals = {};
-protectedStorage.legacyArrayWorktimeFormat = legacyArrayWorktimeFormat;
-
-function legacyArrayWorktimeFormat(worktimes)
-{
-  const arrayTimes = [];
-  for (const wt of Object.keys(worktimes))
+  function legacyArrayWorktimeFormat(worktimes)
+  {
+    const arrayTimes = [];
+    for (const wt of Object.keys(worktimes))
     arrayTimes.push({ name: wt, versions: worktimes[wt].versions });
-  return arrayTimes;
-}
+    return arrayTimes;
+  }
 
-const worktimes = {
-  'map-basic': { versions: ['1.0.0'] },
-  'pyodide': { versions: ['0.23.2'] },
-};
+  const worktimes = {
+    'map-basic': { versions: ['1.0.0'] },
+    'pyodide': { versions: ['0.23.2'] },
+  };
 
-function registerWorktime(name, version)
-{
-  if (!globalThis.worktimes[name])
-    globalThis.worktimes[name] = { versions: [] };
+  function registerWorktime(name, version)
+  {
+    if (!globalThis.worktimes[name])
+      globalThis.worktimes[name] = { versions: [] };
   globalThis.worktimes[name].versions.push(version);
-}
+  }
 
-// nodejs-like environment
-if (typeof module?.exports === 'object')
-  exports.worktimes        = legacyArrayWorktimeFormat(worktimes);
-else // inside the sandbox
-{
-  globalThis.worktimes        = worktimes;
-  globalThis.registerWorktime = registerWorktime;
-}
-
+  // nodejs-like environment
+  if (typeof module?.exports === 'object')
+    exports.worktimes        = legacyArrayWorktimeFormat(worktimes);
+  else // inside the sandbox
+  {
+    globalThis.worktimes        = worktimes;
+    globalThis.registerWorktime = registerWorktime;
+  }
 }
 
 // nodejs-like environment
@@ -53,3 +53,4 @@ if (typeof module?.exports === 'object')
 // inside the sandbox
 else
   self.wrapScriptLoading({ scriptName: 'worktimes' }, worktimes$$fn);
+
