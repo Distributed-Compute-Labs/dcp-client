34cba16250e2868946b9df20499493af72e56ed6
Bryan Hoang <bryan@distributive.network>
2024-03-13 09:03:01 -0400

    fix(configName): allow specifying configName when ran through `node --eval "..."`
    To quickly test config loading, I tried running
    
    ```console
    node --eval "require('.').init({ configName: '...' })"
    ```
    
    but run into an error from `path.resolve` receiving `undefined` as its
    first argument, since `progDir` would be undefined for JS that wasn't
    running from a script.
    
    I find the pattern of using `node --eval` helpful for quickly testing
    the initialization of `dcp-client` under different circumstances, so I
    opted to try and support the pattern through defaulting to resolving
    `progDir` to an empty string, which `path.resolve` treats as
    `process.cwd()`.

--- index.js
+++ index.js
@@ -1066,7 +1066,7 @@ exports.createConfigFragments = async function dcpClient$$createConfigFragments(
   const home = process.env.DCP_HOMEDIR || os.homedir();
   let programName = options.programName;
   const configScope = cliOpts.configScope || process.env.DCP_CONFIG_SCOPE || options.configScope;
-  const progDir = process.mainModule ? path.dirname(process.mainModule.filename) : undefined;
+  const progDir = process.mainModule ? path.dirname(process.mainModule.filename) : process.cwd();
   var   remoteConfig, remoteConfigKVIN;
   const internalConfig = require('dcp/dcp-config'); /* needed to resolve dcpConfig.future - would like to eliminate this /wg */
   const defaultConfig = Object.assign({}, bootstrapConfig);
