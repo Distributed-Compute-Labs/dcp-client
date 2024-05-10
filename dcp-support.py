5f72fb31339bedad40839bd136b6821bf856dfb1
Ryan Saweczko <ryansaweczko@distributive.network>
2024-05-10 10:44:03 -0400

    pm (getRandomValues): properly assign bytes for typed arrays with >1 bytes per element

--- dcp-support.py
+++ dcp-support.py
@@ -14,12 +14,17 @@ def getRandomValues(typedArr):
     setRandomVal = pm.eval("""'use strict';
 function setRandomVal(typedArr, bytes)
 {
-  for (let ind=0; ind < typedArr.length; ind++)
+  const arrBytes = typedArr.BYTES_PER_ELEMENT;
+  for (let i=0; i < typedArr.length; i++)
   {
-    if (typedArr.constructor.name.includes('Big'))
-      typedArr[ind] = BigInt(bytes[ind]);
-    else
-      typedArr[ind] = bytes[ind];
+    const index = i * arrBytes;
+    for (let byte=0; byte < arrBytes; byte++)
+    {
+      if (typedArr.constructor.name.includes('Big'))
+        typedArr[i] += BigInt(bytes[index + byte]) << BigInt(arrBytes - byte - 1) * 8n
+      else
+        typedArr[i] += bytes[index + byte] << (arrBytes - byte - 1) * 8
+    }
   }
 }
 setRandomVal""")
