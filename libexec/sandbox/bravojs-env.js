5ea79184f7eace571a3cd26d9e179a0c2521e15a
Eli <elijah@distributive.network>
2024-05-15 19:33:19 +0000

    Bugfix: Remove .then after non async call

--- libexec/sandbox/bravojs-env.js
+++ libexec/sandbox/bravojs-env.js
@@ -349,7 +349,8 @@ prepPyodide`);
     }
 
     if (error === Symbol.for('workReject')) {
-      reportTimes(metrics).then(() => ring3PostMessage({ request: 'workReject', reason: protectedStorage.workRejectReason }));
+      reportTimes(metrics);
+      ring3PostMessage({ request: 'workReject', reason: protectedStorage.workRejectReason });
     }
     else
       ring3PostMessage({request: 'workError', error: err});
